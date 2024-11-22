e01e93ed1e6c7e00e9b223f96f55a1a5298fb5a7
MaheshKhairnarGNS <mahesh.khairnar777@gmail.com>
2024-11-22 11:00:48 +0530

    initial update 1

--- index.js
+++ index.js
@@ -9,12 +9,37 @@ require('dotenv').config();
 //1. Function Definition
 async function mahiFun() {
     //Every Function return something
-    return await mongoose.connect('mongodb+srv://mahigkhairnar:mahi1212@mahigk-courses-mongodbs.pyqc6.mongodb.net/?retryWrites=true&w=majority&appName=mahigk-courses-MongoDBserver');
+    return await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@mahigk-courses-mongodbs.pyqc6.mongodb.net/?retryWrites=true&w=majority&appName=mahigk-courses-MongoDBserver`);
     }
 
 //2. Function Calling
-  mahiFun().then((e)=>{
+let PromiseObj = mahiFun();
+
+//PO.then().catch()    ------ promise chain
+
+PromiseObj.then((e)=>{
     console.log('DB Connected');
+    
+    //We can do the next task here
+    //1. We have to define schema
+
+    //Lets create the collection/Table
+
+    //Collention Name = Students
+    //object.method()
+    const Students = mongoose.model('Students', {name : String});
+
+    //Students is a class
+
+    const student1 = new Students({name:'Rohit'});
+    student1.save().then(() => console.log('Successfully Save'));
+
+    const student2 = new Students({name:'Shubham'});
+    student2.save().then(() => console.log('Successfully Save'));
+
+    const student3 = new Students({name:'Tushar'});
+    student3.save().then(() => console.log('Successfully Save'));
+
   }).catch(err => console.log(err));
 
 
